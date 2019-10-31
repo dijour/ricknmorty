aa02a46a54f045ed14485945f649404b61111a08
Dean Dijour <dijour@cmu.edu>
2019-10-30 23:12:42 -0400

    preliminary code fixed but still cancer

--- src/Loaders.js
+++ src/Loaders.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import React, {useEffect, useState} from 'react';
 import {Expo, TimelineMax} from "gsap/TweenMax";
@@ -88,10 +89,10 @@ export const createEarthCloud = (containerEarth, earthCloud, earthRadius) => {
     newEarthCloud.material.opacity = 0;
     earthCloud = newEarthCloud;
     return earthCloud
-    // containerEarth.add(earthCloud)
+    containerEarth.add(earthCloud)
 }
 
-export const loadCromulon = (scene, manager, cromulon) => {
+export const loadCromulon = (scene, manager, cromulon, containerEarth) => {
     const cromulonLoader = new STLLoader(manager);
     // cromulonLoader.setDRACOLoader(dracoLoader);
     cromulonLoader.load('/cromulon/model.stl', stl => {
@@ -99,7 +100,7 @@ export const loadCromulon = (scene, manager, cromulon) => {
         stl.scene.position.set(0, 0, 0);
         cromulon = stl.scene;
         cromulon.name = "cromulon"
-        scene.add(cromulon)
+        containerEarth.add(cromulon)
         return cromulon;
     },
         // called when loading has errors
@@ -109,7 +110,59 @@ export const loadCromulon = (scene, manager, cromulon) => {
     );
 }
 
-export const loadRick = (objs, scene, manager, rick) => {
+export const loadButt = (scene, butt, manager, playing, setPlaying, setVideo, domEvents, containerEarth) => {
+    var textureLoader = new THREE.TextureLoader();
+    var map = textureLoader.load('./texture_skin.png');
+    var material = new THREE.MeshPhongMaterial({map: map});
+
+    
+    const buttLoader = new OBJLoader(manager);
+    buttLoader.load('/butt.obj', obj => {
+
+        obj.traverse( function ( node ) {
+
+            if ( node.isMesh ) node.material = material;
+        
+          } );
+
+        obj.scale.set(.5,.5,.5);
+        obj.position.set(100, 50, 120);
+        butt = obj;
+        butt.name = "butt"
+        containerEarth.add(butt)
+
+        domEvents.addEventListener(butt, 'click', function(event){
+            this.tl = new TimelineMax();
+            // this.tl.to(sauser.scale, 3, {x: 1.5, y: 1.5, z: 1.5, ease: Expo.easeOut})
+
+            let originalScale = butt.scale.x;
+            // this.tl.to(sauser.scale, .2, {x: .8, y: .8, z: .8, ease: Expo.easeIn})
+            this.tl.fromTo(butt.scale, 1, {x: originalScale*1.5, y: originalScale*1.5, z: originalScale*1.5, ease: Expo.easeIn}, {x: originalScale, y: originalScale, z: originalScale, ease: Expo.easeOut})
+            // this.tl.to(sauser.scale, .4, {x: originalScale, y: originalScale, z: originalScale, ease: Expo.easeOut})
+        
+            setTimeout(() => {
+                console.log('you clicked on the mesh')
+                if (playing) {
+                    setPlaying(false)
+                }
+                else {
+                    setPlaying(true);
+                }
+                setVideo('butt')
+            }, 1000)
+        }
+        , false)
+    
+        return butt;
+    },
+        // called when loading has errors
+        function ( error ) {
+            console.log( error );
+        }
+    );
+}
+
+export const loadRick = (objs, scene, manager, rick, containerEarth) => {
     const RickWalkLoader = new FBXLoader(manager);
     RickWalkLoader.load('/drunk_idle/Floating.fbx', model => {
         // model is a THREE.Group (THREE.Object3D)                              
@@ -136,7 +189,7 @@ export const loadRick = (objs, scene, manager, rick) => {
     });
 }
 
-export const loadMorty = (scene, manager, morty) => {
+export const loadMorty = (scene, morty, manager, playing, setPlaying, setVideo, domEvents, containerEarth) => {
     const mortyLoader = new GLTFLoader(manager);
     mortyLoader.setDRACOLoader(dracoLoader);
     mortyLoader.load('/morty/scene.gltf', gltf => {
@@ -150,6 +203,30 @@ export const loadMorty = (scene, manager, morty) => {
         gltf.scene.receiveShadow = true;
         morty = gltf.scene;
         morty.name = "morty"
+        scene.add(morty)
+
+        domEvents.addEventListener(morty, 'click', function(event){
+            this.tl = new TimelineMax();
+            // this.tl.to(sauser.scale, 3, {x: 1.5, y: 1.5, z: 1.5, ease: Expo.easeOut})
+
+            let originalScale = morty.scale.x;
+            // this.tl.to(sauser.scale, .2, {x: .8, y: .8, z: .8, ease: Expo.easeIn})
+            this.tl.fromTo(morty.scale, 1, {x: originalScale*1.5, y: originalScale*1.5, z: originalScale*1.5, ease: Expo.easeIn}, {x: originalScale, y: originalScale, z: originalScale, ease: Expo.easeOut})
+            // this.tl.to(sauser.scale, .4, {x: originalScale, y: originalScale, z: originalScale, ease: Expo.easeOut})
+        
+            setTimeout(() => {
+                console.log('you clicked on the mesh')
+                if (playing) {
+                    setPlaying(false)
+                }
+                else {
+                    setPlaying(true);
+                }
+                setVideo('morty')
+            }, 1000)
+        }
+        , false)
+
         return morty;
     },
         // called when loading has errors
@@ -159,7 +236,7 @@ export const loadMorty = (scene, manager, morty) => {
     );
 }
 
-export const loadTinyPlanet = (scene, manager, tinyPlanet) => {
+export const loadTinyPlanet = (scene, tinyPlanet, manager, playing, setPlaying, setVideo, domEvents, containerEarth) => {
     const mortyLoader = new GLTFLoader(manager);
     mortyLoader.setDRACOLoader(dracoLoader);
     mortyLoader.load('/tiny_planet/scene.gltf', gltf => {
@@ -173,6 +250,30 @@ export const loadTinyPlanet = (scene, manager, tinyPlanet) => {
         gltf.scene.receiveShadow = true;
         tinyPlanet = gltf.scene;
         tinyPlanet.name = "morty"
+        scene.add(tinyPlanet)
+
+        domEvents.addEventListener(tinyPlanet, 'click', function(event){
+            this.tl = new TimelineMax();
+            // this.tl.to(sauser.scale, 3, {x: 1.5, y: 1.5, z: 1.5, ease: Expo.easeOut})
+
+            let originalScale = tinyPlanet.scale.x;
+            // this.tl.to(sauser.scale, .2, {x: .8, y: .8, z: .8, ease: Expo.easeIn})
+            this.tl.fromTo(tinyPlanet.scale, 1, {x: originalScale*1.5, y: originalScale*1.5, z: originalScale*1.5, ease: Expo.easeIn}, {x: originalScale, y: originalScale, z: originalScale, ease: Expo.easeOut})
+            // this.tl.to(sauser.scale, .4, {x: originalScale, y: originalScale, z: originalScale, ease: Expo.easeOut})
+        
+            setTimeout(() => {
+                console.log('you clicked on the mesh')
+                if (playing) {
+                    setPlaying(false)
+                }
+                else {
+                    setPlaying(true);
+                }
+                setVideo('tinyPlanet')
+            }, 1000)
+        }
+        , false)
+
         return tinyPlanet;
     },
         // called when loading has errors
@@ -182,7 +283,7 @@ export const loadTinyPlanet = (scene, manager, tinyPlanet) => {
     );
 }
 
-export const loadPickleRick = (scene, pickleRick, manager, domEvents) => {
+export const loadPickleRick = (scene, pickleRick, manager, playing, setPlaying, setVideo, domEvents, containerEarth) => {
     const pickleRickLoader = new GLTFLoader(manager);
     pickleRickLoader.setDRACOLoader(dracoLoader);
     pickleRickLoader.load('/pickleRick/scene.gltf', gltf => {
@@ -190,13 +291,36 @@ export const loadPickleRick = (scene, pickleRick, manager, domEvents) => {
         for (const anim of gltf.animations) {
             mixer.clipAction(anim).play();
         }
-        gltf.scene.scale.set(1,1,1);
+        gltf.scene.scale.set(4,4,4);
         gltf.scene.position.set(50, 0, 100);
         gltf.scene.castShadow = true;
         gltf.scene.receiveShadow = true;
         pickleRick = gltf.scene;
         pickleRick.name = "pickleRick"
         scene.add(pickleRick)
+
+        domEvents.addEventListener(pickleRick, 'click', function(event){
+            this.tl = new TimelineMax();
+            // this.tl.to(sauser.scale, 3, {x: 1.5, y: 1.5, z: 1.5, ease: Expo.easeOut})
+
+            let originalScale = pickleRick.scale.x;
+            // this.tl.to(sauser.scale, .2, {x: .8, y: .8, z: .8, ease: Expo.easeIn})
+            this.tl.fromTo(pickleRick.scale, 1, {x: originalScale*1.5, y: originalScale*1.5, z: originalScale*1.5, ease: Expo.easeIn}, {x: originalScale, y: originalScale, z: originalScale, ease: Expo.easeOut})
+            // this.tl.to(sauser.scale, .4, {x: originalScale, y: originalScale, z: originalScale, ease: Expo.easeOut})
+        
+            setTimeout(() => {
+                console.log('you clicked on the mesh')
+                if (playing) {
+                    setPlaying(false)
+                }
+                else {
+                    setPlaying(true);
+                }
+                setVideo('pickleRick')
+            }, 1000)
+        }
+        , false)
+
         return(pickleRick)
     },
         // called when loading has errors
@@ -228,12 +352,9 @@ export const loadSauser = (containerEarth, sauser, manager, playing, setPlaying,
     
         domEvents.addEventListener(sauser, 'click', function(event){
             this.tl = new TimelineMax();
-            // this.tl.to(sauser.scale, 3, {x: 1.5, y: 1.5, z: 1.5, ease: Expo.easeOut})
 
             let originalScale = sauser.scale.x;
-            // this.tl.to(sauser.scale, .2, {x: .8, y: .8, z: .8, ease: Expo.easeIn})
             this.tl.fromTo(sauser.scale, 1, {x: originalScale*1.5, y: originalScale*1.5, z: originalScale*1.5, ease: Expo.easeIn}, {x: originalScale, y: originalScale, z: originalScale, ease: Expo.easeOut})
-            // this.tl.to(sauser.scale, .4, {x: originalScale, y: originalScale, z: originalScale, ease: Expo.easeOut})
         
             setTimeout(() => {
                 console.log('you clicked on the mesh')
@@ -258,7 +379,7 @@ export const loadSauser = (containerEarth, sauser, manager, playing, setPlaying,
     );
 }
 
-export const loadGun = (scene, gun, manager) => {
+export const loadGun = (scene, gun, manager, playing, setPlaying, setVideo, domEvents, containerEarth) => {
     const gunLoader = new GLTFLoader(manager);
     gunLoader.setDRACOLoader(dracoLoader);
     gunLoader.load('/portal_gun/scene.gltf', gltf => {
@@ -272,6 +393,30 @@ export const loadGun = (scene, gun, manager) => {
         gltf.scene.receiveShadow = true;
         gun = gltf.scene;
         gun.name = "gun"
+        scene.add(gun)
+
+        domEvents.addEventListener(gun, 'click', function(event){
+            this.tl = new TimelineMax();
+            // this.tl.to(sauser.scale, 3, {x: 1.5, y: 1.5, z: 1.5, ease: Expo.easeOut})
+
+            let originalScale = gun.scale.x;
+            // this.tl.to(sauser.scale, .2, {x: .8, y: .8, z: .8, ease: Expo.easeIn})
+            this.tl.fromTo(gun.scale, 1, {x: originalScale*1.5, y: originalScale*1.5, z: originalScale*1.5, ease: Expo.easeIn}, {x: originalScale, y: originalScale, z: originalScale, ease: Expo.easeOut})
+            // this.tl.to(sauser.scale, .4, {x: originalScale, y: originalScale, z: originalScale, ease: Expo.easeOut})
+        
+            setTimeout(() => {
+                console.log('you clicked on the mesh')
+                if (playing) {
+                    setPlaying(false)
+                }
+                else {
+                    setPlaying(true);
+                }
+                setVideo('gun')
+            }, 1000)
+        }
+        , false)
+
         return gun;
     },
         // called when loading has errors
@@ -282,7 +427,7 @@ export const loadGun = (scene, gun, manager) => {
 }
 
 
-export const loadPlumbus = (scene, plumbus, manager, playing, setPlaying, setVideo, domEvents) => {
+export const loadPlumbus = (scene, plumbus, manager, playing, setPlaying, setVideo, domEvents, containerEarth) => {
     const plumbusLoader = new GLTFLoader(manager);
     plumbusLoader.setDRACOLoader(dracoLoader);
     plumbusLoader.load('/plumbus/scene.gltf', gltf => {
@@ -298,10 +443,11 @@ export const loadPlumbus = (scene, plumbus, manager, playing, setPlaying, setVid
         plumbus = gltf.scene;
         plumbus.userData = { name: "plumbus" }
         plumbus.name = "plumbus"
+        scene.add(plumbus)
+        // containerEarth.add(plumbus)
         
         domEvents.addEventListener(plumbus, 'click', function(event){
             this.tl = new TimelineMax();
-            // this.tl.to(sauser.scale, 3, {x: 1.5, y: 1.5, z: 1.5, ease: Expo.easeOut})
 
             let originalScale = plumbus.scale.x;
 
@@ -330,7 +476,7 @@ export const loadPlumbus = (scene, plumbus, manager, playing, setPlaying, setVid
     );
 }
 
-export const loadDisco = (scene, discoBall, manager, playing, setPlaying, setVideo, domEvents) => {
+export const loadDiscoBall = (scene, discoBall, manager, playing, setPlaying, setVideo, domEvents, containerEarth) => {
     const discoLoader = new GLTFLoader(manager);
     discoLoader.setDRACOLoader(dracoLoader);
     discoLoader.load('/disco_ball/scene.gltf', gltf => {
@@ -338,18 +484,18 @@ export const loadDisco = (scene, discoBall, manager, playing, setPlaying, setVid
         for (const anim of gltf.animations) {
             mixer.clipAction(anim).play();
         }
-        gltf.scene.scale.set(5,5,5);
-        gltf.scene.position.set(90, 20, 0);
-        gltf.scene.rotation.set(0, 0, Math.PI/2)
+        gltf.scene.scale.set(.5,.5,.5);
+        gltf.scene.position.set(150, 20, 0);
+        gltf.scene.rotation.set(0, 0, 0)
         gltf.scene.castShadow = true;
         gltf.scene.receiveShadow = true;
         discoBall = gltf.scene;
         discoBall.userData = { name: "discoBall" }
         discoBall.name = "discoBall"
+        containerEarth.add(discoBall)
         
         domEvents.addEventListener(discoBall, 'click', function(event){
             this.tl = new TimelineMax();
-            // this.tl.to(sauser.scale, 3, {x: 1.5, y: 1.5, z: 1.5, ease: Expo.easeOut})
 
             let originalScale = discoBall.scale.x;
 
@@ -378,7 +524,7 @@ export const loadDisco = (scene, discoBall, manager, playing, setPlaying, setVid
     );
 }
 
-export const loadMicrophone = (scene, microphone, manager, playing, setPlaying, setVideo, domEvents) => {
+export const loadMicrophone = (scene, microphone, manager, playing, setPlaying, setVideo, domEvents, containerEarth) => {
     const microphoneLoader = new GLTFLoader(manager);
     microphoneLoader.setDRACOLoader(dracoLoader);
     microphoneLoader.load('/microphone/scene.gltf', gltf => {
@@ -386,14 +532,15 @@ export const loadMicrophone = (scene, microphone, manager, playing, setPlaying, 
         for (const anim of gltf.animations) {
             mixer.clipAction(anim).play();
         }
-        gltf.scene.scale.set(5,5,5);
-        gltf.scene.position.set(90, 20, 0);
-        gltf.scene.rotation.set(0, 0, Math.PI/2)
+        gltf.scene.scale.set(.2,.2,.2);
+        gltf.scene.position.set(-90, 20, 0);
+        gltf.scene.rotation.set(0, 0, 0)
         gltf.scene.castShadow = true;
         gltf.scene.receiveShadow = true;
         microphone = gltf.scene;
         microphone.userData = { name: "microphone" }
         microphone.name = "microphone"
+        containerEarth.add(microphone)
         
         domEvents.addEventListener(microphone, 'click', function(event){
             this.tl = new TimelineMax();
@@ -423,22 +570,23 @@ export const loadMicrophone = (scene, microphone, manager, playing, setPlaying, 
     );
 }
 
-export const loadTV = (scene, TV, manager, playing, setPlaying, setVideo, domEvents) => {
+export const loadTV = (scene, TV, manager, playing, setPlaying, setVideo, domEvents, containerEarth) => {
     const TVLoader = new GLTFLoader(manager);
     TVLoader.setDRACOLoader(dracoLoader);
-    TVLoader.load('/TV/scene.gltf', gltf => {
+    TVLoader.load('/tv/scene.gltf', gltf => {
         const mixer = new THREE.AnimationMixer(gltf.scene);
         for (const anim of gltf.animations) {
             mixer.clipAction(anim).play();
         }
-        gltf.scene.scale.set(5,5,5);
-        gltf.scene.position.set(90, 20, 0);
-        gltf.scene.rotation.set(0, 0, Math.PI/2)
+        gltf.scene.scale.set(10,10,10);
+        gltf.scene.position.set(-150, 20, 0);
+        gltf.scene.rotation.set(0, 0, 0)
         gltf.scene.castShadow = true;
         gltf.scene.receiveShadow = true;
         TV = gltf.scene;
         TV.userData = { name: "TV" }
         TV.name = "TV"
+        containerEarth.add(TV)
         
         domEvents.addEventListener(TV, 'click', function(event){
             this.tl = new TimelineMax();
@@ -453,11 +601,57 @@ export const loadTV = (scene, TV, manager, playing, setPlaying, setVideo, domEve
                 else {
                     setPlaying(true);
                 }
-                setVideo('microphone')
+                setVideo('TV')
             }, 1000)}
         , false)
 
         return TV
+
+        },
+        // called when loading has errors
+        function ( error ) {
+            console.log( error );
+        }
+        
+    );
+}
+
+export const loadSun = (scene, sun, manager, playing, setPlaying, setVideo, domEvents, containerEarth) => {
+    const sunLoader = new GLTFLoader(manager);
+    sunLoader.setDRACOLoader(dracoLoader);
+    sunLoader.load('/sun/scene.gltf', gltf => {
+        const mixer = new THREE.AnimationMixer(gltf.scene);
+        for (const anim of gltf.animations) {
+            mixer.clipAction(anim).play();
+        }
+        gltf.scene.scale.set(5,5,5);
+        gltf.scene.position.set(150, 50, 0);
+        gltf.scene.rotation.set(0, 0, 0)
+        gltf.scene.castShadow = true;
+        gltf.scene.receiveShadow = true;
+        sun = gltf.scene;
+        sun.userData = { name: "sun" }
+        sun.name = "sun"
+        containerEarth.add(sun)
+        
+        domEvents.addEventListener(sun, 'click', function(event){
+            this.tl = new TimelineMax();
+            let originalScale = sun.scale.x;
+            this.tl.to(sun.scale, .15, {x: originalScale*1.5, y: originalScale*1.5, z: originalScale*1.5, ease: Expo.easeIn})
+            this.tl.to(sun.scale, .4, {x: originalScale, y: originalScale, z: originalScale, ease: Expo.easeOut})
+            setTimeout(() => {
+                console.log('you clicked on the mesh')
+                if (playing) {
+                    setPlaying(false)
+                }
+                else {
+                    setPlaying(true);
+                }
+                setVideo('sun')
+            }, 1000)}
+        , false)
+
+        return sun
 
         },
         // called when loading has errors
